c8156c7b6c025317481bc6ff4c85972bb5ca1c51
Jovany Leandro G.C <bit4bit@riseup.net>
2025-11-09 20:42:49 +0200

    initial

---
+++ lib/ecto_elk.ex
@@ -0,0 +1,18 @@
+defmodule EctoElk do
+  @moduledoc """
+  Documentation for `EctoElk`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoElk.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
